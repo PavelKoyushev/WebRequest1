74bf96f94b95f31fbf5f0b7801af0dcf51b0ca89
Pavel Koyushev <pkoyushev@gmail.com>
2017-06-23 10:41:08 +0300

    commit

--- WebRequest сервера/Program.cs
+++ WebRequest сервера/Program.cs
@@ -9,7 +9,7 @@ namespace WebRequest_сервера
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// Главная точка входа для приложения
         /// </summary>
         [STAThread]
         static void Main()
